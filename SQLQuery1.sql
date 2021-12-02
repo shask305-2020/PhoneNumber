5c6d6d0524c83602f8dd40efe245e77065005e5a
ШАСК Студент <shask305@hotmail.com>
2021-12-02 10:33:25 +0500

    02.12.2021 003

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Contact] (
+    [ContactID]    INT            IDENTITY (1, 1) NOT NULL,
+    [Name]         NVARCHAR (50)  NULL,
+    [MobileNumber] NVARCHAR (50)  NULL,
+    [Address]      NVARCHAR (MAX) NULL,
+    PRIMARY KEY CLUSTERED ([ContactID] ASC)
+);
+
