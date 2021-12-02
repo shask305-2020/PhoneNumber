4609a13b58a0691b3761aa37d98eee9ab38d7d9b
ШАСК Студент <shask305@hotmail.com>
2021-12-02 10:23:02 +0500

    02.12.2021 001

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -183,6 +183,7 @@ namespace PhoneNumber
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
 
 namespace PhoneNumber
 {
     public partial class Form1 : Form
     {
+        SqlConnection sqlConnection = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\toxa_\source\repos\PhoneNumber\Database.mdf;Integrated Security=True");
+        
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
