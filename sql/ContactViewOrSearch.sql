f222958fbce013d45bebbcd482877f4207e98cbc
ШАСК Студент <shask305@hotmail.com>
2021-12-03 10:13:03 +0500

    03.12.2021 004

--- sql/ContactViewOrSearch.sql
+++
@@ -1,6 +0,0 @@
-CREATE PROCEDURE [dbo].[ContactViewOrSearch]
-	@ContactName nvarchar(50)
-	AS
-	SELECT *
-	FROM Contact
-	WHERE name LIKE @ContactName+'%'
--- sql/Table.Contact.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Contact] (
-    [ContactID]    INT            IDENTITY (1, 1) NOT NULL,
-    [Name]         NVARCHAR (50)  NULL,
-    [MobileNumber] NVARCHAR (50)  NULL,
-    [Address]      NVARCHAR (MAX) NULL,
-    PRIMARY KEY CLUSTERED ([ContactID] ASC)
-);
-
