62f1ff19f9cf64d6a97d70bfd6f406f15367c240
ШАСК Студент <shask305@hotmail.com>
2021-12-03 10:11:24 +0500

    03.12.2021 002

--- sql/dbo.Table.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Contact]
-(
-	[ContactID] INT NOT NULL PRIMARY KEY, 
-    [Name] NVARCHAR(50) NULL, 
-    [MobileNumber] NVARCHAR(50) NULL, 
-    [Address] NVARCHAR(MAX) NULL
-)
